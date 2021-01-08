323734337c27f6fd217e49092d80ff1c57f426d0
ThomasFinnern <finnern_at_wt@tomfinnern.de>
2021-01-08 17:32:27 +0100

    Small improvements

--- Src/NetGrepWin/Program/CmdLine/clsCmdLineConfig.cs
+++ Src/NetGrepWin/Program/CmdLine/clsCmdLineConfig.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace CmdLine2005
 {
-    class clsCmdLineConfig
+    public class clsCmdLineConfig
     {
         //---------------------------------------------------------------------------------------------
         // Config variables 
